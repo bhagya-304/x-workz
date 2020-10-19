51cc4637c125690a33b0dcef44e0cb71592940c1
bhagya-304 <72971409+bhagya-304@users.noreply.github.com>
2020-10-19 21:14:07 +0530

    Third Commit

---
+++ DataType.java
@@ -0,0 +1,66 @@
+class BooleanExample {
+    public static void main(String[] args) {
+    	
+        boolean flag = true;
+        System.out.println(flag);
+    }
+}
+class ByteExample {
+  public static void main(String[] args) {
+    byte myNum = 100;
+    System.out.println(myNum);  
+  }
+}
+
+class ShortExample {
+    public static void main(String[] args) {
+    	
+        short temperature;
+        temperature = -200;
+        System.out.println(temperature);
+    }
+}
+class LongExample {
+    public static void main(String[] args) {
+    	
+        long noofpopulation = -42332200000L;
+        System.out.println(noofpopulation);
+    }
+}
+class CharExample {
+    public static void main(String[] args) {
+    	
+        char letter1 = '9';
+        System.out.println(letter1);
+    	
+        char letter2 = 65;
+        System.out.println(letter2);
+
+    }
+}
+class FloatExample {
+    public static void main(String[] args) {
+    	
+        float number = -42.3f;
+        System.out.println(number);
+    }
+}
+class IntExample {
+  public static void main(String[] args){
+    int x = 1, y = 5;
+    System.out.println("x");
+    System.out.println("y");
+    System.out.println("x"+"y");
+    System.out.println(x);
+    System.out.println(y);
+    System.out.println(x+y);
+  }
+}
+ class DoubleExample  { 
+    public static void main(String args[]) {
+        double d1 = 594.25;
+        double d2 = 95.246;
+        double d3 =d1+d2;
+        System.out.println("Addition:=" +d3); 
+    } 
+}
